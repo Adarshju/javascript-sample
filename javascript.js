16a6a1a3d878eaad9ca8c28f834e2cd7c63b1ec3
KIDILLAM GAMING <108668941+Adarshju@users.noreply.github.com>
2024-06-26 00:18:28 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,48 +1,48 @@
-// // sum of two number
-// const num1 = prompt("Enter the first number:  For Sum");
-// const num2 = prompt("Enter the second number: For Sum");
-// const sum = parseFloat(num1) + parseInt(num2);
-// console.log("sum of two numbers  " + sum);
+// sum of two number
+const num1 = prompt("Enter the first number:  For Sum");
+const num2 = prompt("Enter the second number: For Sum");
+const sum = parseFloat(num1) + parseInt(num2);
+console.log("sum of two numbers  " + sum);
 
-// //rectangle area calculation
+//rectangle area calculation
 
-// const width = parseInt(prompt("Enter The Rectangle width  For area calculation"));
-// const length = parseInt(prompt("Enter The Rectangle length  For area calculation"));
-// const area = width * length;
-// console.log("Area Of The Rectangle " + area);
+const width = parseInt(prompt("Enter The Rectangle width  For area calculation"));
+const length = parseInt(prompt("Enter The Rectangle length  For area calculation"));
+const area = width * length;
+console.log("Area Of The Rectangle " + area);
 
-// // voting eligiblity checking
+// voting eligiblity checking
 
-// const age = parseInt(prompt("Enter The Age For Voting: "))
+const age = parseInt(prompt("Enter The Age For Voting: "))
 
-// if(age>=18){
-//     console.log("You Are Eligible For Voting");
-// }else{
-//     console.log("You Are Not Eligible For Voting");
-// }
+if(age>=18){
+    console.log("You Are Eligible For Voting");
+}else{
+    console.log("You Are Not Eligible For Voting");
+}
 
-// // pass or failed
+// pass or failed
 
-// const grade = parseInt(prompt("Enter Your Grade :"))
-// if (grade>=60) {
-//     console.log("You Passed");
-// }else{
-//     console.log("You Failed");
-// }
+const grade = parseInt(prompt("Enter Your Grade :"))
+if (grade>=60) {
+    console.log("You Passed");
+}else{
+    console.log("You Failed");
+}
 
-// // Zero Or Postive
+// Zero Or Postive
 
-// const num = parseInt(
-//   prompt("Enter a  Number For Checking Postive OR Negative")
-// );
+const num = parseInt(
+  prompt("Enter a  Number For Checking Postive OR Negative")
+);
 
-// if (num > 0) {
-//   console.log("Your Enter Number " + num + " Is Positive");
-// } else if (num < 0) {
-//   console.log("Your Enter Number " + num + " Is Negative");
-// } else {
-//   console.log("Your Enter Number " + num + " Is Zero");
-// }
+if (num > 0) {
+  console.log("Your Enter Number " + num + " Is Positive");
+} else if (num < 0) {
+  console.log("Your Enter Number " + num + " Is Negative");
+} else {
+  console.log("Your Enter Number " + num + " Is Zero");
+}
 
 // // Checking Your Age child or Above
 
@@ -62,19 +62,3 @@ if (age_check < 0) {
   console.log("You Are Senior");
 }
 
-// // square of Each number
-
-// const number = [1, 3, 9, 12, 15, 18, 21];
-//   const squares= number.map(num=>num**2);
-//   console.log("The Squres Of the Array  number are : "+squares);
-
-// const limit = parseInt(prompt("Enter the limit: "), 10);
-// let sumOfOdds = 0;
-
-// for (let i = 1; i <= limit; i++) {
-//     if (i % 2 !== 0) {
-//         sumOfOdds += i;
-//     }
-// }
-
-// console.log("The sum of all odd numbers up to", limit, "is:", sumOfOdds);
